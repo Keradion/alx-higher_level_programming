f7c4d95dacb1b220528f85e75c100dcd48b3154a
Daniel Berihun <danielshitaye10@gmail.com>
2024-03-12 06:53:42 -0700

    fizz buzz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def fizzbuzz():
+    for each in range(1, 101):
+        if each % 3 == 0:
+            print('{} '.format('Fizz'), end='')
+        elif each % 5 == 0:
+            print('{} '.format('Buzz'), end='')
+        elif each % 3 == 0 and each % 5 == 0:
+            print('{} '.format('FizzBuzz'), end='')
+        else:
+            print('{} '.format(each), end='')
