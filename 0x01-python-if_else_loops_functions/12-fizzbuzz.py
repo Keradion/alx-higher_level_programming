0e7c7365a798f603de98322afa9664f4a561efb7
Daniel Berihun <danielshitaye10@gmail.com>
2024-03-13 00:45:41 -0700

    fizz buzz text format

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,10 +3,10 @@
 def fizzbuzz():
     for each in range(1, 101):
         if each % 3 == 0:
-            print('{}'.format('Fizz'), end=' ')
+            print('{}'.format('Fizz '), end='')
         elif each % 5 == 0:
-            print('{}'.format('Buzz'), end=' ')
+            print('{}'.format('Buzz '), end='')
         elif each % 3 == 0 and each % 5 == 0:
-            print('{}'.format('FizzBuzz'), end='')
+            print('{}'.format('FizzBuzz '), end='')
         else:
-            print('{}'.format(each), end=' ')
+            print('{} '.format(each), end='')
