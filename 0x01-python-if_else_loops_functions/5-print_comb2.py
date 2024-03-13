17997d6f835817bfbadff4040dacd45d469ee599
Daniel Berihun <danielshitaye10@gmail.com>
2024-03-13 01:57:54 -0700

    padding specifier

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
 for each in range(0, 100):
-    if each < 10:
-        print('{}{}, '.format(0, each), end='')
-    elif each == 99:
-        print(99)
+    if each == 99:
+        print('{}'.format(each))
     else:
-        print('{}, '.format(each), end='')
+        print('{:2d}, '.format(each), end='')
