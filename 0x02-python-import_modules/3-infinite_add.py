48e506ecbd72bb4dac9650399a62b004ba43b71f
Daniel Berihun <danielshitaye10@gmail.com>
2024-03-14 07:26:10 -0700

    indentation error correction

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,13 +3,11 @@
 import sys
 
 if __name__ == '__main__':
-sum_of_arguments = 0
+    sum_of_arguments = 0
+    if len(sys.argv) == 1:
+        print('{}'.format(0))
+        exit()
 
-if len(sys.argv) == 1:
-    print('{}'.format(0))
-    exit()
-
-for each in range(1, len(sys.argv)):
-    sum_of_arguments = sum_of_arguments + int(sys.argv[each])
-
+    for each in range(1, len(sys.argv)):
+        sum_of_arguments = sum_of_arguments + int(sys.argv[each])
 print('{}'.format(sum_of_arguments))
