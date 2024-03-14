a7d1445f5de2eb0afb5d8baf5054ffeb5402ad71
Daniel Berihun <danielshitaye10@gmail.com>
2024-03-14 06:45:17 -0700

    command line arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
         print('{} arguments.'.format(0))
     elif len(sys.argv) == 2:
         print('{} argument:'.format(1))
-        print('{}: {}'.format(each, sys.argv[1]))
+        print('{}: {}'.format(1, sys.argv[1]))
     else:
         print('{} arguments:'.format(len(sys.argv) - 1))
         for each in range(1, len(sys.argv)):
