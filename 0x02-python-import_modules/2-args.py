249d679900413121f3e45687812cfba6e4f065bf
Daniel Berihun <danielshitaye10@gmail.com>
2024-03-14 06:43:59 -0700

    command line arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,12 +4,13 @@ import sys
 ''' arguments stored in argv attribute of the sys module '''
 
 if __name__ == '__main__':
-    for each in range(len(sys.argv) - 1):
-        if len(sys.argv) == 1:
-            print('{} arguments.'.format(0))
-        elif len(sys.argv) == 2:
-            print('{} argument:'.format(1))
-            print('{}: {}'.format(1, sys.argv[1]))
-            break
-        else:
-            print('{}: {}'.format(each + 1, sys.argv[each + 1]))
+
+    if len(sys.argv) == 1:
+        print('{} arguments.'.format(0))
+    elif len(sys.argv) == 2:
+        print('{} argument:'.format(1))
+        print('{}: {}'.format(each, sys.argv[1]))
+    else:
+        print('{} arguments:'.format(len(sys.argv) - 1))
+        for each in range(1, len(sys.argv)):
+            print('{}: {}'.format(each, sys.argv[each]))
