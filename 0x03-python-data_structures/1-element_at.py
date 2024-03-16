d673037db916c92c56ba16e711fa7edc9eec5bfa
Daniel Berihun <danielshitaye10@gmail.com>
2024-03-16 02:49:17 -0700

    task 1 retrieve element at index idx

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,6 +2,6 @@
 
 def element_at(my_list, idx):
     ''' Retrieve an element from given index '''
-    if idx < 0 or idx > len(my_list):
+    if idx < 0 or idx > (len(my_list) - 1) or my_list == []:
         return None
     return my_list[idx]
