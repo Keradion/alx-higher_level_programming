b5ee928ee77f13902c33683e2f3f901a846ad684
Daniel Berihun <danielshitaye10@gmail.com>
2024-03-17 03:29:25 -0700

    find multiples of 2 in a list

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,10 +2,12 @@
 
 def divisible_by_2(my_list=[]):
     ''' find multiples of 2 in a list '''
+    if my_list == []:
+        return None
     new_list = my_list[:]
     for each in range(0, len(my_list)):
         if my_list[each] % 2 == 0:
-            new_list[each] = 'True'
+            new_list[each] = True
         else:
-            new_list[each] = 'False'
+            new_list[each] = False
     return new_list
