7af496b92eccf56e951f969cb9d8e7adefa4c132
Daniel Berihun <danielshitaye10@gmail.com>
2024-03-17 02:32:00 -0700

    return max intger in a list

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    ''' find max item in a list '''
+    max_item = 0
+
+    if my_list == []:
+        return None
+
+    for each in range(0, (len(my_list) - 1)):
+        if my_list[each] > max_item:
+            max_item = my_list[each]
+    return max_item
