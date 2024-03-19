122256ab8947185b6d2c8a03f27c66cae2f4d5e1
Daniel Berihun <danielshitaye10@gmail.com>
2024-03-19 01:45:01 -0700

    handlin empty list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,7 +2,9 @@
 
 def print_reversed_list_integer(my_list=[]):
     ''' Print elements of a list in reverse '''
-    if my_list == [] or if my_list is None:
+    if my_list == []:
         return
+    if my_list is None:
+        return None
     for each in range((len(my_list) - 1), -1, -1):
         print('{:d}'.format(my_list[each]))
