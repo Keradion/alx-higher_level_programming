56bd6ee310f00995ca76ef7dfa97d9c61e087ec1
Daniel Berihun <danielshitaye10@gmail.com>
2024-03-20 05:24:45 -0700

    max value in a dictionary

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+    ''' Returns a key with the biggest value '''
+    max_score = 0
+    max_score_key = 'a'
+
+    if a_dictionary is None:
+        return None
+
+    for key, value in a_dictionary.items():
+        if value > max_score:
+            max_score = value
+            max_score_key = key
+    return max_score_key
